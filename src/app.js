2313a51bfda4bea014cbbae7c8c2da91ad47d694
Carmelico92 <130740279+Carmelico92@users.noreply.github.com>
2024-02-12 12:12:54 +0000

    Generador de Excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function excuseGenerator() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = `${whoRandom}  ${actionRandom}  ${whatRandom}  ${whenRandom}`;
+  //return `${whoRandom}  ${actionRandom}  ${whatRandom}  ${whenRandom}`;
+  document.getElementById("excuse").innerHTML = excuse;
 };
